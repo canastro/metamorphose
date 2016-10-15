47b4f0a54b437d017bd5cb1f18d4cf6d61b95453
Canastro <ricardo.canastro@blip.pt>
2016-10-15 18:55:54 +0100

    Update examples

---
+++ examples/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const assert = require('assert');
+const SchemaObject = require('../lib/schema-object');
+
+const Base = new SchemaObject({
+    brand: {
+        type: String,
+        key: 'brand',
+        transform: (brand) => brand.toUpperCase()
+    },
+    isPrivate: 'isPrivate'
+});
+
+const Spec = new SchemaObject({
+    size: {
+        key: 'size',
+        transform: (size) => `${size}"`
+    },
+    connectors: {
+        key: ':root',
+        transform: (tv) => (tv.videoConnectors + tv.audioConnectors).split(', ')
+    }
+});
+
+const Tv = Base.extends({
+    specs: { key: ':root', type: Spec },
+    price: { key: 'price', defaults: 'N/A' }
+});
+
+const expected = {
+    brand: 'GRUNDIG',
+    isPrivate: true,
+    price: 'N/A',
+    specs: {
+        size: '42"',
+        connectors: [
+            '1 x Coaxial Digital Audio',
+            '1 x Component Video',
+            '1 x Composite Video',
+            '2 x HDMI1 x Headphone',
+            '1 x PC Audio In',
+            '1 x RCA Audio L+R'
+        ]
+    }
+};
+
+const original = {
+    brand: 'Grundig',
+    thisShouldBeIgnored: 'HEY',
+    isPrivate: true,
+    size: 42,
+    quality: 'Full HD',
+    videoConnectors: '1 x Coaxial Digital Audio, 1 x Component Video, 1 x Composite Video, 2 x HDMI',
+    audioConnectors: '1 x Headphone, 1 x PC Audio In, 1 x RCA Audio L+R'
+};
+
+const result = Tv.create(original);
+console.log(result.obj);
+assert.deepEqual(expected, result.obj);
+console.log('success');
